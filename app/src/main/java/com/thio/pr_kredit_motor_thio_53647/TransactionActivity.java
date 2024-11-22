a97710c59d19279524c7023e43ac230c4e78d1b7
Thio Van <thiovan99@gmail.com>
2024-11-22 12:54:51 +0700

    WIP Transaction Activity

--- app/src/main/java/com/thio/pr_kredit_motor_thio_53647/TransactionActivity.java
+++ app/src/main/java/com/thio/pr_kredit_motor_thio_53647/TransactionActivity.java
@@ -1,24 +1,67 @@
 package com.thio.pr_kredit_motor_thio_53647;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
 
 public class TransactionActivity extends AppCompatActivity {
+
+    Button btPengajuanKredit, btPembayaranAngsuran, btDataPengajuanKredit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_transaction);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btPengajuanKredit = (Button) findViewById(R.id.btPengajuanKredit);
+        btPembayaranAngsuran = (Button) findViewById(R.id.btPembayaranAngsuran);
+        btDataPengajuanKredit = (Button) findViewById(R.id.btDataPengajuanKredit);
+
+        btPengajuanKredit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                KlikbtPengajuanKredit();
+            }
         });
+
+        btPembayaranAngsuran.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                KlikbtPembayaranAngsuran();
+            }
+        });
+
+        btDataPengajuanKredit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                KlikbtDataPengajuanKredit();
+            }
+        });
+
     }
+
+    public void KlikbtPengajuanKredit() {
+        Intent i = new Intent(getApplicationContext(), PengajuanKreditActivity.class);//target = nama class
+        startActivity(i);
+    }
+
+    public void KlikbtPembayaranAngsuran() {
+        Intent i = new Intent(getApplicationContext(),
+                PembayaranAngsuranActivity.class);//target = nama class
+        startActivity(i);
+    }
+
+    public void KlikbtDataPengajuanKredit() {
+        Intent i = new Intent(getApplicationContext(),
+                DataPengajuanKreditActivity.class);//target = nama class
+        startActivity(i);
+    }
+
+
 }
