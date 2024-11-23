e760cd038a64569495521e95451508021ae5e1ac
Thio Van <thiovan99@gmail.com>
2024-11-23 21:14:21 +0700

    Add Tab Icon & Change Background Color

--- app/src/main/java/com/thio/pr_kredit_motor_thio_53647/MainActivity.java
+++ app/src/main/java/com/thio/pr_kredit_motor_thio_53647/MainActivity.java
@@ -21,20 +21,41 @@ public class MainActivity extends TabActivity {
         intent = new Intent().setClass(this, HomeActivity.class);
 
         // inisialisasi TabSpec untuk tab yg lain dan menambahkan ke TabHost
-        spec = tabHost.newTabSpec("Home").setIndicator("Home" ).setContent(intent);
+        spec = tabHost.newTabSpec("Home")
+                .setIndicator("", res.getDrawable(R.drawable.ic_tab_home))
+                .setContent(intent);
         tabHost.addTab(spec);
 
         // Melakukan kepada tab yang lain
         intent = new Intent().setClass(this, TransactionActivity.class);
-        spec = tabHost.newTabSpec("Transaction").setIndicator("Transaction")
+        spec = tabHost.newTabSpec("Transaction")
+                .setIndicator("", res.getDrawable(R.drawable.ic_tab_tansaksi))
                 .setContent(intent);
         tabHost.addTab(spec);
 
         intent = new Intent().setClass(this, AboutActivity.class);
-        spec = tabHost.newTabSpec("About").setIndicator("About")
+        spec = tabHost.newTabSpec("About")
+                .setIndicator("", res.getDrawable(R.drawable.ic_tab_about))
                 .setContent(intent);
         tabHost.addTab(spec);
         tabHost.setCurrentTab(3);
 
+        tabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String tabId) {
+                changeTabBgColor(tabHost);
+            }
+        });
+
+        changeTabBgColor(tabHost);
+
+    }
+
+    public void changeTabBgColor(TabHost tabHost) {
+        for (int i = 0; i < tabHost.getTabWidget().getChildCount(); i++) {
+            tabHost.getTabWidget().getChildAt(i).setBackgroundColor(getResources().getColor(R.color.red_700));
+        }
+
+        tabHost.getTabWidget().getChildAt(tabHost.getCurrentTab()).setBackgroundColor(getResources().getColor(R.color.red_500));
     }
 }
